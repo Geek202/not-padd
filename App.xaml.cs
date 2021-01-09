221d58d31eba583cc22675456bd4192ea2253bac
Tom_The_Geek <23531706+Geek202@users.noreply.github.com>
2021-01-09 20:05:45 +0000

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace padd
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+        public static String message;
+
+        protected override void OnStartup(StartupEventArgs e) {
+            foreach (var message in e.Args) {
+                var win = new padd.MainWindow(message);
+                win.Show();
+            }
+
+            if (e.Args.Length == 0) {
+                var win = new padd.MainWindow("");
+                win.Show();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace padd
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(String message)
+        {
+            InitializeComponent();
+            Console.WriteLine(App.message);
+            var box = (this.FindName("text") as TextBox);
+            box.Text = message;
+            box.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            box.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            box.Focus();
+        }
+
+        private void ButtonClicked(object sender, RoutedEventArgs e) {
+            MessageBox.Show("hello, world!");
+        }
+    }
+}
